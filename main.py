cea90f7601d9ec311499f946db18c0d4952aac9e
tombo314 <u_tombo@icloud.com>
2023-04-18 11:19:58 +0900

    [Add] is_n_percent()

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from pprint import pprint
+from random import randint
 import tkinter as tk
 import json
 
 class Battle:
-    def __init__(self) -> None:
+    def __init__(self):
         self.name_box = [0]*3
         self.name_text = [0]*3
         self.hp_box = [0]*3
@@ -11,7 +12,7 @@ class Battle:
         self.mp_box = [0]*3
         self.mp_text = [0]*3
     
-    def make_element(self, party: list, type_: str, is_enemy: bool) -> None:
+    def make_element(self, party: list, type_: str, is_enemy: bool):
         """
         party: 敵か味方のモンスター3体の要素の配列
         type: 「名前」「HP」「MP」のいずれか
@@ -70,6 +71,30 @@ class Battle:
                 )
             i += 1
 
+def battle(party_enemy: list, party_friend: list):
+    """
+    バトルを行う
+    party_enemy: 敵パーティー
+    party_friend: 味方パーティー
+    """
+    btl = Battle()
+    btl.make_element(party_enemy, "名前", True)
+    btl.make_element(party_enemy, "HP", True)
+    btl.make_element(party_enemy, "MP", True)
+    btl.make_element(party_friend, "名前", False)
+    btl.make_element(party_friend, "HP", False)
+    btl.make_element(party_friend, "MP", False)
+
+def is_n_percent(prob: int):
+    """
+    probパーセントの確率でTrueを返す
+    prob: Trueが返ってくる確率(0~100)
+    """
+    r = randint(1, 100)
+    if r<=prob:
+        return True
+    return False
+
 with open("data.json", encoding="utf-8") as f:
     data = json.load(f)
     monster = data["monster"]
@@ -84,28 +109,16 @@ canvas = tk.Canvas(
 )
 canvas.pack()
 
-def battle(party_friend: list, party_enemy: list):
-    """
-    
-    """
-    btl = Battle()
-    btl.make_element(party_enemy, "名前", True)
-    btl.make_element(party_enemy, "HP", True)
-    btl.make_element(party_enemy, "MP", True)
-    btl.make_element(party_friend, "名前", False)
-    btl.make_element(party_friend, "HP", False)
-    btl.make_element(party_friend, "MP", False)
-
 battle([
         monster["スライム"],
         monster["ドラキー"],
         monster["ゴースト"]
     ],
     [
-        monster["ドラキー"],
         monster["スライム"],
+        monster["ドラキー"],
         monster["ゴースト"]
     ]
 )
 
-app.mainloop()
+# app.mainloop()
