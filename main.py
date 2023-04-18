498e0106581d6435b1e4e39f95b871eccde6094c
tombo314 <u_tombo@icloud.com>
2023-04-18 13:47:22 +0900

    [Update] UIのインスタンスの持ち方の変更、バトル時のパラメータの追加 など

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pprint import pprint
-from random import randint
+from random import randint, uniform
 import tkinter as tk
 import json
 import numpy as np
@@ -13,85 +13,117 @@ class Battle:
         """
         # UIを生成する
         # 0がenemy, 1がfriend
-        self.name_box = [[0, 0]]*3
-        self.name_text = [[0, 0]]*3
-        self.hp_box = [[0, 0]]*3
-        self.hp_text = [[0, 0]]*3
-        self.mp_box = [[0, 0]]*3
-        self.mp_text = [[0, 0]]*3
-        self.__make_element(enemy, "名前", False)
-        self.__make_element(enemy, "HP", False)
-        self.__make_element(enemy, "MP", False)
-        self.__make_element(friend, "名前", True)
-        self.__make_element(friend, "HP", True)
-        self.__make_element(friend, "MP", True)
+        self.name_box = [
+            {mons["name"]: 0 for mons in enemy},
+            {mons["name"]: 0 for mons in friend}
+        ]
+        self.name_text = [
+            {mons["name"]: 0 for mons in enemy},
+            {mons["name"]: 0 for mons in friend}
+        ]
+        self.hp_box = [
+            {mons["name"]: 0 for mons in enemy},
+            {mons["name"]: 0 for mons in friend}
+        ]
+        self.hp_text = [
+            {mons["name"]: 0 for mons in enemy},
+            {mons["name"]: 0 for mons in friend}
+        ]
+        self.mp_box = [
+            {mons["name"]: 0 for mons in enemy},
+            {mons["name"]: 0 for mons in friend}
+        ]
+        self.mp_text = [
+            {mons["name"]: 0 for mons in enemy},
+            {mons["name"]: 0 for mons in friend}
+        ]
+        self.__make_element(enemy, "name", False)
+        self.__make_element(enemy, "hp", False)
+        self.__make_element(enemy, "mp", False)
+        self.__make_element(friend, "name", True)
+        self.__make_element(friend, "hp", True)
+        self.__make_element(friend, "mp", True)
         # バトル時のパラメータ
-        self.
+        self.hp = [
+            {mons["name"]: mons["hp"] for mons in enemy},
+            {mons["name"]: mons["hp"] for mons in friend}
+        ]
+        self.mp = [
+            {mons["name"]: mons["mp"] for mons in enemy},
+            {mons["name"]: mons["mp"] for mons in friend}
+        ]
+        self.attack = [
+            {mons["name"]: mons["attack"] for mons in enemy},
+            {mons["name"]: mons["attack"] for mons in friend}
+        ]
+        self.magic_attack = [
+            {mons["name"]: mons["magic_attack"] for mons in enemy},
+            {mons["name"]: mons["magic_attack"] for mons in friend}
+        ]
+        self.deffense = [
+            {mons["name"]: mons["deffense"] for mons in enemy},
+            {mons["name"]: mons["deffense"] for mons in friend}
+        ]
+        self.agility = [
+            {mons["name"]: mons["agility"] for mons in enemy},
+            {mons["name"]: mons["agility"] for mons in friend}
+        ]
     
     def __make_element(self, party: list, type_: str, is_friend: bool) -> None:
         """
         敵と味方のUIを生成する
         party: 敵か味方のモンスター3体の要素の配列
-        type: 「名前」「HP」「MP」のいずれか
+        type: 「name」「hp」「mp」のいずれか
         is_enemy: 敵の要素であるかどうか
         """
         y = -1
         color = ""
-        if type_=="名前":
+        if type_=="name":
             y = 100
-        elif type_=="HP":
+        elif type_=="hp":
             y = 160
-        elif type_=="MP":
+        elif type_=="mp":
             y = 220
         if is_friend==True:
-            color = "#f33"
-        elif is_friend==False:
             color = "#3f3"
             y += 250
+        elif is_friend==False:
+            color = "#f33"
         i = 0
+        # パーティーのモンスターのUIを表示する
         for mons in party:
             start_x, start_y = 220*i+100, y
             width, height = 160, 40
             end_x, end_y = start_x+width, start_y+height
-            if type_=="名前":
-                self.name_box[i][is_friend] = canvas.create_rectangle(
-                    start_x, start_y,
-                    end_x, end_y,
-                    fill = "#ddd",
-                    outline = color
-                )
-                self.name_text[i][is_friend] = canvas.create_text(
-                    (start_x+end_x)/2, (start_y+end_y)/2,
-                    text = mons["name"]
-                )
-            elif type_=="HP":
-                self.hp_box[i][is_friend] = canvas.create_rectangle(
-                    start_x, start_y,
-                    end_x, end_y,
-                    fill = "#ddd",
-                    outline = color
-                )
-                hp = mons["hp"]
-                self.hp_text[i][is_friend] = canvas.create_text(
-                    (start_x+end_x)/2, (start_y+end_y)/2,
-                    text = f"{hp} / {hp}"
-                )
-            elif type_=="MP":
-                self.mp_box[i][is_friend] = canvas.create_rectangle(
-                    start_x, start_y,
-                    end_x, end_y,
-                    fill = "#ddd",
-                    outline = color
-                )
-                mp = mons["mp"]
-                self.mp_text[i][is_friend] = canvas.create_text(
-                    (start_x+end_x)/2, (start_y+end_y)/2,
-                    text = f"{mp} / {mp}"
-                )
+            # 四角形を表示する
+            self.name_box[is_friend][mons[type_]] = canvas.create_rectangle(
+                start_x, start_y,
+                end_x, end_y,
+                fill = "#ddd",
+                outline = color
+            )
+            content = ""
+            if type_=="name":
+                content = mons[type_]
+            elif type_=="hp" or type_=="mp":
+                content = f"{mons[type_]} / {mons[type_]}"
+            # テキストを表示する
+            self.name_text[is_friend][mons[type_]] = canvas.create_text(
+                (start_x+end_x)/2, (start_y+end_y)/2,
+                text = content
+            )
             i += 1
 
-    def battle_start():
-        
+    def battle_start_auto(self):
+        """
+        バトルを開始する（自動）
+        """
+        pass
+    
+    def battle_start_manual(self):
+        """
+        バトルを開始する（手動）
+        """
 
 def battle(party_enemy: list, party_friend: list) -> None:
     """
@@ -100,6 +132,7 @@ def battle(party_enemy: list, party_friend: list) -> None:
     party_friend: 味方のパーティーの配列
     """
     btl = Battle(party_enemy, party_friend)
+    btl.battle_start_auto()
 
 def is_n_percent(prob: int) -> bool:
     """
