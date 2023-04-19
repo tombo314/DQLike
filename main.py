074288653781c23dbefa279ce3050525a6e811c5
tombo314 <u_tombo@icloud.com>
2023-04-19 22:36:47 +0900

    Update monster[name]["name"], __revive_monster() など

--- main.py
+++ main.py
@@ -17,28 +17,28 @@ class Battle:
         self.friend = friend
         # 0がenemyのUI, 1がfriendのUI
         self.name_box = [
-            {mons["name"]: 0 for mons in enemy},
-            {mons["name"]: 0 for mons in friend}
+            {monster[name]["name"]: 0 for name in enemy},
+            {monster[name]["name"]: 0 for name in friend}
         ]
         self.name_text = [
-            {mons["name"]: 0 for mons in enemy},
-            {mons["name"]: 0 for mons in friend}
+            {monster[name]["name"]: 0 for name in enemy},
+            {monster[name]["name"]: 0 for name in friend}
         ]
         self.hp_box = [
-            {mons["name"]: 0 for mons in enemy},
-            {mons["name"]: 0 for mons in friend}
+            {monster[name]["name"]: 0 for name in enemy},
+            {monster[name]["name"]: 0 for name in friend}
         ]
         self.hp_text = [
-            {mons["name"]: 0 for mons in enemy},
-            {mons["name"]: 0 for mons in friend}
+            {monster[name]["name"]: 0 for name in enemy},
+            {monster[name]["name"]: 0 for name in friend}
         ]
         self.mp_box = [
-            {mons["name"]: 0 for mons in enemy},
-            {mons["name"]: 0 for mons in friend}
+            {monster[name]["name"]: 0 for name in enemy},
+            {monster[name]["name"]: 0 for name in friend}
         ]
         self.mp_text = [
-            {mons["name"]: 0 for mons in enemy},
-            {mons["name"]: 0 for mons in friend}
+            {monster[name]["name"]: 0 for name in enemy},
+            {monster[name]["name"]: 0 for name in friend}
         ]
         # モンスタ―とUIの座標の対応関係
         # (monster_name, is_friend): (start_x, start_y)
@@ -52,55 +52,33 @@ class Battle:
         self.__make_party(friend, "mp", True)
         # バトル時のパラメータ
         self.hp = [
-            {mons["name"]: mons["hp"] for mons in enemy},
-            {mons["name"]: mons["hp"] for mons in friend}
+            {monster[name]["name"]: monster[name]["hp"] for name in enemy},
+            {monster[name]["name"]: monster[name]["hp"] for name in friend}
         ]
         self.mp = [
-            {mons["name"]: mons["mp"] for mons in enemy},
-            {mons["name"]: mons["mp"] for mons in friend}
+            {monster[name]["name"]: monster[name]["mp"] for name in enemy},
+            {monster[name]["name"]: monster[name]["mp"] for name in friend}
         ]
         self.attack = [
-            {mons["name"]: mons["attack"] for mons in enemy},
-            {mons["name"]: mons["attack"] for mons in friend}
+            {monster[name]["name"]: monster[name]["attack"] for name in enemy},
+            {monster[name]["name"]: monster[name]["attack"] for name in friend}
         ]
         self.magic_attack = [
-            {mons["name"]: mons["magic_attack"] for mons in enemy},
-            {mons["name"]: mons["magic_attack"] for mons in friend}
+            {monster[name]["name"]: monster[name]["magic_attack"] for name in enemy},
+            {monster[name]["name"]: monster[name]["magic_attack"] for name in friend}
         ]
         self.deffense = [
-            {mons["name"]: mons["deffense"] for mons in enemy},
-            {mons["name"]: mons["deffense"] for mons in friend}
+            {monster[name]["name"]: monster[name]["deffense"] for name in enemy},
+            {monster[name]["name"]: monster[name]["deffense"] for name in friend}
         ]
         self.agility = [
-            {mons["name"]: mons["agility"] for mons in enemy},
-            {mons["name"]: mons["agility"] for mons in friend}
+            {monster[name]["name"]: monster[name]["agility"] for name in enemy},
+            {monster[name]["name"]: monster[name]["agility"] for name in friend}
         ]
         self.dead = [
-            {mons["name"]: False for mons in enemy},
-            {mons["name"]: False for mons in friend}
+            {monster[name]["name"]: False for name in enemy},
+            {monster[name]["name"]: False for name in friend}
         ]
-    
-    def __draw_ui(self, start_x, start_y, type_: str, is_friend: bool) -> None:
-        pass
-        # width, height = 160, 40
-        # end_x, end_y = start_x+width, start_y+height
-        # # 四角形を表示する
-        # self.name_box[is_friend][mons[type_]] = canvas.create_rectangle(
-        #     start_x, start_y,
-        #     end_x, end_y,
-        #     fill = "#ddd",
-        #     outline = color
-        # )
-        # content = ""
-        # if type_=="name":
-        #     content = mons[type_]
-        # elif type_=="hp" or type_=="mp":
-        #     content = f"{mons[type_]} / {mons[type_]}"
-        # # テキストを表示する
-        # self.name_text[is_friend][mons[type_]] = canvas.create_text(
-        #     (start_x+end_x)/2, (start_y+end_y)/2,
-        #     text = content
-        # )
     
     def __make_party(self, party: list[str], type_: str, is_friend: bool) -> None:
         """
@@ -124,14 +102,14 @@ class Battle:
             color = "#f33"
         i = 0
         # パーティーのモンスターのUIを表示する
-        for mons in party:
+        for name in party:
             start_x, start_y = 220*i+100, y
             # モンスター名とモンスターの敵・味方の区分と、UIの座標を対応付ける
-            self.elem_coord.update({(mons["name"], type_, is_friend): (start_x, start_y)})
+            self.elem_coord.update({(name, type_, is_friend): (start_x, start_y)})
             width, height = 160, 40
             end_x, end_y = start_x+width, start_y+height
             # 四角形を表示する
-            self.name_box[is_friend][mons[type_]] = canvas.create_rectangle(
+            self.name_box[is_friend][monster[name][type_]] = canvas.create_rectangle(
                 start_x, start_y,
                 end_x, end_y,
                 fill = "#ddd",
@@ -139,11 +117,11 @@ class Battle:
             )
             content = ""
             if type_=="name":
-                content = mons[type_]
+                content = monster[name][type_]
             elif type_=="hp" or type_=="mp":
-                content = f"{mons[type_]} / {mons[type_]}"
+                content = f"{monster[name][type_]} / {monster[name][type_]}"
             # テキストを表示する
-            self.name_text[is_friend][mons[type_]] = canvas.create_text(
+            self.name_text[is_friend][monster[name][type_]] = canvas.create_text(
                 (start_x+end_x)/2, (start_y+end_y)/2,
                 text = content
             )
@@ -155,59 +133,55 @@ class Battle:
         monster_name: モンスターの名前
         is_friend: 味方であるかどうか
         """
+        canvas.update()
+        sleep(1)
         self.dead[is_friend][monster_name] = True
         canvas.delete(self.name_box[is_friend][monster_name])
+        canvas.delete(self.name_text[is_friend][monster_name])
     
-    def __revive_monster(self, monster_name: str, is_friend: bool) -> None:
+    def __revive_monster(self, name: str, is_friend: bool) -> None:
         """
         モンスターが復活する
-        monster_name: モンスターの名前
+        name: モンスターの名前
         is_friend: 味方であるかどうか
         """
-        # self.dead[is_friend][monster_name] = False
-        # width, height = 160, 40
-        # start_x, start_y = self.elem_coord[]
-        # end_x, end_y = start_x+width, start_y+height
-        # # 四角形を表示する
-        # self.name_box[is_friend][mons[type_]] = canvas.create_rectangle(
-        #     start_x, start_y,
-        #     end_x, end_y,
-        #     fill = "#ddd",
-        #     outline = color
-        # )
-        # content = ""
-        # if type_=="name":
-        #     content = mons[type_]
-        # elif type_=="hp" or type_=="mp":
-        #     content = f"{mons[type_]} / {mons[type_]}"
-        # # テキストを表示する
-        # self.name_text[is_friend][mons[type_]] = canvas.create_text(
-        #     (start_x+end_x)/2, (start_y+end_y)/2,
-        #     text = content
-        # )
+        canvas.update()
+        sleep(1)
+        self.dead[is_friend][name] = False
+        width, height = 160, 40
+        start_x, start_y = self.elem_coord[(name, "name", is_friend)]
+        end_x, end_y = start_x+width, start_y+height
+        if is_friend:
+            color = "#3f3"
+        else:
+            color = "#f33"
+        # 四角形を表示する
+        self.name_box[is_friend][name] = canvas.create_rectangle(
+            start_x, start_y,
+            end_x, end_y,
+            fill = "#ddd",
+            outline = color
+        )
+        # テキストを表示する
+        self.name_text[is_friend][name] = canvas.create_text(
+            (start_x+end_x)/2, (start_y+end_y)/2,
+            text = name
+        )
 
     def battle_start_auto(self) -> None:
         """
         バトルを開始する（自動）
         """
         # 両方のチームで1体以上のモンスターが生きている場合、バトルを継続する
-        while any([self.dead[0][mons["name"]]==False for mons in self.enemy]) and any([self.dead[1][mons["name"]]==False for mons in self.friend]):
+        while any([self.dead[0][monster[name]["name"]]==False for name in self.enemy]) and any([self.dead[1][monster[name]["name"]]==False for name in self.friend]):
             # 各ターンの処理
             order = []
-            for mons in self.enemy:
+            for name in self.enemy:
                 r = uniform(0.8, 1.2)
-                order.append([r*mons["agility"], mons["name"], "enemy"])
-            for mons in self.friend:
-                order.append([r*mons["agility"], mons["name"], "friend"])
+                order.append([r*monster[name]["agility"], monster[name]["name"], "enemy"])
+            for name in self.friend:
+                order.append([r*monster[name]["agility"], monster[name]["name"], "friend"])
             order.sort(reverse=True)
-            canvas.update()
-            sleep(1)
-            canvas.delete(self.name_text[0]["スライム"])
-            canvas.delete(self.name_box[0]["スライム"])
-            canvas.update()
-            sleep(1)
-            canvas.delete(self.name_text[1]["スライム"])
-            canvas.delete(self.name_box[1]["スライム"])
             break
     
     def battle_start_manual(self) -> None:
@@ -241,17 +215,6 @@ def select_skill(skill: dict) -> str:
     """
     return choice(list(skill.keys()), p=list(skill.values()))
 
-def calc_damage(skill_name: str, attack: int, magic_attack: int, deffense: int, attribute_damage: dict, is_physics: bool) -> int:
-    """
-    ダメージを計算する
-    skill_name: スキル名
-    attack: 物理攻撃力
-    magic_attack: 呪文攻撃力
-    deffense: 物理防御力
-    attribute_damage: 属性耐性
-    is_physics: 物理攻撃かどうか
-    """
-
 def param_level_up(param: int) -> int:
     """
     パラメータを強化する
@@ -274,16 +237,9 @@ canvas = tk.Canvas(
 canvas.pack()
 
 if 1:
-    battle([
-            monster["スライム"],
-            monster["ドラキー"],
-            monster["ゴースト"]
-        ],
-        [
-            monster["スライム"],
-            monster["ドラキー"],
-            monster["ボストロール"]
-        ]
+    battle(
+        ["スライム", "ドラキー", "ゴースト"],
+        ["スライム", "ドラキー", "ボストロール"]
     )
     app.mainloop()
 
@@ -292,3 +248,4 @@ if 1:
 
 ・モンスターの重複禁止
 """
+
