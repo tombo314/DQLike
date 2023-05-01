c799c996ef40c7689dfadc3628bb303d106850f0
tombo314 <u_tombo@icloud.com>
2023-05-01 11:02:05 +0900

    Add webp2png.py

---
+++ webp2png.py
@@ -0,0 +1,7 @@
+# from glob import glob
+# from PIL import Image
+
+# files = glob("images/webp/*")
+# for file in files:
+#     img = Image.open(file)
+#     img.save(file[:7]+"png/"+file[12:-5]+".png")
