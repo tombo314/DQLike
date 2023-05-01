6b53caf7188ec539a1b55cbfaf3534880e2e8210
tombo314 <u_tombo@icloud.com>
2023-05-01 11:42:09 +0900

    Update widthを条件分岐

--- image_resize.py
+++ image_resize.py
@@ -6,14 +6,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-width = 90
-height = 90
+width = None
+height = None
 
 files = glob("images/png/*")
 for file in files:
     name = file[11:-4]
     img = Image.open(file)
     img = np.array(img)
+    if name=="ドラキー":
+        width = 100
+    else:
+        width = 100
+    height = 90
     img = cv2.resize(img, dsize=[width, height])
     img = Image.fromarray(img)
     save_path = f"images/png_resized/{name}_resized.png"
