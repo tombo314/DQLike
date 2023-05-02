e2d3a1dfb0800524608e5bbcf574b3c5237d412f
tombo314 <u_tombo@icloud.com>
2023-05-02 16:17:02 +0900

    Add image_process.py

---
+++ image_process.py
@@ -0,0 +1,30 @@
+from glob import glob
+from PIL import Image
+from PIL import Image
+from glob import glob
+import cv2
+import numpy as np
+
+files = glob("images/webp/*")
+for file in files:
+    img = Image.open(file)
+    img.save(file[:7]+"png/"+file[12:-5]+".png")
+
+
+width = None
+height = None
+
+files = glob("images/png/*")
+for file in files:
+    name = file[11:-4]
+    img = Image.open(file)
+    img = np.array(img)
+    if name=="ドラキー" or name=="ボストロール":
+        width = 130
+    else:
+        width = 90
+    height = 90
+    img = cv2.resize(img, dsize=[width, height])
+    img = Image.fromarray(img)
+    save_path = f"images/png_resized/{name}_resized.png"
+    img.save(save_path)
