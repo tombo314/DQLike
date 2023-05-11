c7bda0ffdea6d19479a47fa71ad4a4e16a81a97d
tombo314 <u_tombo@icloud.com>
2023-05-11 23:44:08 +0900

    Add 主人公の写真の大きさをカスタマイズ

--- image_process.py
+++ image_process.py
@@ -20,9 +20,27 @@ for file in files:
     img = np.array(img)
     if name=="ドラキー" or name=="ボストロール":
         width = 130
+    elif name[:5]=="主人公J3":
+        width = 60
     else:
         width = 90
-    height = 90
+    if name[:5]=="主人公J3":
+        height = 160
+    elif name[:5]=="主人公IX":
+        height = 140
+    elif name[:7]=="主人公VIII":
+        height = 160
+    elif name[:5]=="主人公VI" and name[:6]!="主人公VII":
+        height = 140
+    elif name[:4]=="主人公V":
+        height = 100
+    elif name[:5]=="主人公IV":
+        height = 140
+    elif name[:3]=="主人公":
+        # 主人公のデフォルトheight
+        height = 120
+    else:
+        height = 90
     img = cv2.resize(img, dsize=[width, height])
     img = Image.fromarray(img)
     save_path = f"images/png_resized/{name}_resized.png"
