f80c61ad74103ec206d21ed2f6fe70674400e3c8
tombo314 <u_tombo@icloud.com>
2023-05-26 00:07:56 +0900

    Remove クラスのインポートを消去 など

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
-# クラスのインポート
+# クラスをインポート
 from user_info import user_info
-from window import window
-from battle import battle
-from fusion import fusion
 from map import map
 
 # 味方パーティーを設定
 user_info.set_friend(["スライム", "ドラキー", "ギュメイ将軍"])
-
-# 味方パーティーを設定
-battle.set_friend(user_info.friend)
-# # 敵パーティーを設定
-# battle.set_enemy(["スライム", "ゴースト", "ゲルニック将軍"])
-# battle.init_param()
-# battle.draw_ui()
 
 # マップを表示
 map.run()
@@ -21,8 +11,11 @@ map.run()
 """
 To Do
 
+・ゲルニック将軍を実装する
+    -> ルカナン
 """
 """
-Memo
+メモ
 
+・自傷ダメージのある攻撃は、range=singleのみ
 """
