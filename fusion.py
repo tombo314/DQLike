f6f7851a6c94a313a512a2e5f4194b076ce8ab88
tombo314 <u_tombo@icloud.com>
2023-05-25 09:21:26 +0900

    Add Fusionクラス

---
+++ fusion.py
@@ -0,0 +1,20 @@
+class Fusion:
+    """
+    モンスターを配合する
+    """
+    def __init__(self) -> None:
+        """
+        モンスターの配合に関する情報を持つ
+        """
+    
+    def show_fusion_screen(self) -> None:
+        """
+        配合画面を表示する
+        """
+    
+    def get_makeable_monster(self, monster: str) -> None:
+        """
+        モンスターの配合候補を返す
+        monster: 親の片方となるモンスター
+        """
+        return fusion_tree[monster]
