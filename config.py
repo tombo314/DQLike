f3232a6bc1d1e6070a035a2a6ff52444e5935afb
tombo314 <u_tombo@icloud.com>
2023-05-25 14:35:59 +0900

    Add DURATIONの設定

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
 # バトル時の表示速度を設定
 is_fast = 1
+
+if is_fast==0:
+    SHOW_DURATION = 1.7
+    BATTLE_START_DURATION = 1
+elif is_fast==1:
+    SHOW_DURATION = 0.05
+    BATTLE_START_DURATION = 0.5
+BATTLE_FINISH_DURATION = 2
