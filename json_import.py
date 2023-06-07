8ce39c10aca1f9296912b75c3f73fd5dfe1c5d80
tombo314 <u_tombo@icloud.com>
2023-06-08 08:31:17 +0900

    Update JSONデータの読み込みを関数にする

--- json_import.py
+++ json_import.py
@@ -1,12 +1,22 @@
 # ライブラリのインポート
 import json
 
-# JSONデータを読み込む
-with open("data/monster.json", encoding="utf-8") as data:
-    monster = json.load(data)
-with open("data/user.json", encoding="utf-8") as data:
-    user = json.load(data)
-with open("data/skill.json", encoding="utf-8") as data:
-    skill = json.load(data)
-with open("data/fusion_tree.json", encoding="utf-8") as data:
-    fusion_tree = json.load(data)
+def import_json(save_data_id: int) -> None:
+    """
+    JSONデータを読み込む
+    save_data_id: セーブデータの番号(1～3)
+    """
+    with open(f"data/save_data/save_data_{save_data_id}.json", encoding="utf-8") as data:
+        save_data = json.load(data)
+    with open("data/monster.json", encoding="utf-8") as data:
+        monster = json.load(data)
+    with open("data/skill.json", encoding="utf-8") as data:
+        skill = json.load(data)
+    with open("data/fusion_tree.json", encoding="utf-8") as data:
+        fusion_tree = json.load(data)
+    return {
+        "save_data": save_data,
+        "monster": monster,
+        "skill": skill,
+        "fusion_tree": fusion_tree
+    }
