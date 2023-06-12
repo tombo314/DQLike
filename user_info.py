8573162c7239f8fe2689bf8d1d5f060da31809ed
tombo314 <u_tombo@icloud.com>
2023-06-12 13:38:42 +0900

    Update コメントを変更・追加、self.friendの初期値を変更

--- user_info.py
+++ user_info.py
@@ -3,13 +3,14 @@ class UserInfo:
     プレイヤーの情報を管理する
     """
     def __init__(self) -> None:
-        # 味方パーティーの連想配列
-        self.friend = [None]*3
+        # 味方パーティー
+        self.friend = []
 
     def set_friend(self, friend: list[dict]) -> None:
         """
-        自分のパーティーを設定する
+        味方パーティーを設定する
         """
+        # 味方パーティーを設定する
         self.friend = friend.copy()
         # パーティー内でモンスターの重複があったら終了
         a = []
